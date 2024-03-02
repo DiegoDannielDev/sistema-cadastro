13f6770170d8b960c4ea6415a6664c0f7d0a03fa
diegodanniel <diego.diniz.dev@gmail.com>
2024-03-02 01:05:16 -0300

    incluido conexão com o banco incluido entities incluido configuração de base de dados

---
+++ src/main/java/br/com/arquite/infraestrutura/jpa/entities/ClienteDocumentoEntity.java
@@ -0,0 +1,16 @@
+package br.com.arquite.infraestrutura.jpa.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class ClienteDocumentoEntity {
+
+  private CpfEntity cpfDomain;
+  private RegistroUnicoEntity rg;
+}
